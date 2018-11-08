1a4f4092f6c17c64b9e11f178ce8ca90371e7a19
Clayton Winsor <claytonwinsor@me.com>
2018-11-08 17:25:54 -0500

    Backend setup

--- src/main/java/moco/qiesbackend/BackEnd.java
+++ src/main/java/moco/qiesbackend/BackEnd.java
@@ -2,5 +2,17 @@ package moco.qiesbackend;
 
 public class BackEnd {
 
+    public static void main(String[] args) {
+        int exitCode = 0;
 
+        if (args.length != 4) {
+            log.log(Level.SEVERE, "Incorrect number of arguments " + args.length);
+            exitCode = 1;
+            System.exit(exitCode);
+        }
+
+        // Do parsing and file stuff here
+
+        System.exit(exitCode);
+    }
 }
