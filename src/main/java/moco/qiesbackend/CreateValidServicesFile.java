f2b18950010a8609a92d82b9ab186f1146557c85
Otto <otto.breski@gmail.com>
2018-11-09 20:00:46 -0500

    Added an addService Method Header
    And a couple lines including return

--- src/main/java/moco/qiesbackend/CreateValidServicesFile.java
+++ src/main/java/moco/qiesbackend/CreateValidServicesFile.java
@@ -1,5 +1,14 @@
 package moco.qiesbackend;
 
+import java.io.File;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+
 public class CreateValidServicesFile {
 
+    public String addService(String validServicesFilePath, String service) {
+        File validServicesFile = new File(validServicesFilePath);
+        return validServicesFilePath;
+    }
 }
